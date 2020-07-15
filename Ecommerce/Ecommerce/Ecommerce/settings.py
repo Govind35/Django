edd56efe37be57582e2349c02e1d768a1edf5991
govind35 <govind.kalsa35@gmail.com>
2020-07-15 21:21:43 +0530

    ..

--- Ecommerce/Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/Ecommerce/settings.py
@@ -125,12 +125,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')    
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')    
+#]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MADIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images ')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/images')
 
 
 
+ 
