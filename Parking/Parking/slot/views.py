50cfcfb193827b59f56b04aa3e9700eeb3de75a1
govind35 <govind.kalsa35@gmail.com>
2020-07-30 22:01:16 +0530

    created .env file

--- Parking/Parking/slot/views.py
+++ Parking/Parking/slot/views.py
@@ -7,13 +7,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from . models import Parking
 from . serializers import slotseri
+from decouple import config
 
 
 # Park the car 
 class slotList(APIView):
     def post(self, request, car_no):
         try:
-            if(Parking.objects.all().count() <= 50):
+            if(Parking.objects.all().count() <= config('Size')):
                 freeSpace = Parking.objects.filter(car='NA').first()
                 if(freeSpace):
                     freeSpace.car = car_no
@@ -38,8 +39,6 @@ class slotList(APIView):
         
         except Exception:
             return Response({'Error':'PARKING IS FULL 2'},status=status.HTTP_404_NOT_FOUND)
-
-
 
 # Remove Car
 class freeSlot(APIView):
@@ -67,9 +66,9 @@ class check(APIView):
                 serialiser = slotseri(slotFound,many=True)
             
             else:
-                return Response({'Message':'Invalid Credentials'},status=status.HTTP_404_NOT_FOUND)
+                return Response({'Message':'Invalid Credentials 1'},status=status.HTTP_404_NOT_FOUND)
 
             return Response(serialiser.data,status=status.HTTP_200_OK)
         
         except Exception :
-            return Response({'Message':'Invalid Credentials'},status=status.HTTP_404_NOT_FOUND)
+            return Response({'Message':'Invalid Credentials 2'},status=status.HTTP_404_NOT_FOUND)
